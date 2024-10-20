0aa9e8d6d11aa8fba785b43218baf008259b104e
MohamedAklamaash <aklamaash78@gmail.com>
2024-10-20 15:55:31 +0530

    done eda for the dataset

--- src/exception.py
+++ src/exception.py
@@ -10,8 +10,8 @@ def errorMessageDetails(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = errorMessageDetails(error_message,error_detail)
+        super.__init__(error_message)
+        self.error_message = errorMessageDetails(error=error_message,error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
+from src.logger import logging
 import sys
-import logging
-from exception import CustomException
+from src.exception import CustomException
+
+
 if __name__ == "__main__":
     try:
-        a = 1/0
+        a = 1 / 0
     except ZeroDivisionError as e:
-        logging.error("Division by zero",exc_info=True)
-        raise CustomException(e,sys)
+        logging.error("Division by zero error occurred", exc_info=True)
+        raise CustomException(e, sys)
