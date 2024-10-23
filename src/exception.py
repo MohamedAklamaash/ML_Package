e58d3f3b86998d9251e29535ea645548d20bc935
MohamedAklamaash <aklamaash78@gmail.com>
2024-10-23 12:38:56 +0530

    data transformation done

--- src/exception.py
+++ src/exception.py
@@ -10,7 +10,7 @@ def errorMessageDetails(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = errorMessageDetails(error=error_message,error_detail=error_detail)
     
     def __str__(self):
